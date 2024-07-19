e4c4ffcf6b856365991a112836bcc26138623a8f
melvinlee770 <melvinlee0199@gmail.com>
2024-07-19 21:52:06 +0800

    done for the topBox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flexBox-container">
+      <div className="topBox">
+        <h1 className="topBox-style" id="topBox-title">
+          Join our community
+        </h1>
+        <h2 className="topBox-style" id="topBox-subtitle">
+          30-day, hassle-free money back guarantee
+        </h2>
+        <h3 className="topBox-style" id="topBox-body">
+          Gain access to our full library of tutorials along with expert code
+          reviews. Perfect for any developers who are serious about honing their
+          skills.
+        </h3>
+      </div>
+      <div className="bottomLeftBox">
+        <h2>Monthly Subscription</h2>
+      </div>
+      <div className="bottomRightBox"></div>
     </div>
   );
 }
